da22af3b3ced33a436eb6b2f33779a4889097435
duguangyan <dis2010@126.com>
2018-12-07 17:25:45 +0800

    init

--- public/js/style.js
+++ public/js/style.js
@@ -1,29 +1,4 @@
 $(function () {
-    /**
-     * 首页轮播图
-     * @type {Swiper}
-     */
-    var mySwiper = new Swiper ('.swiper-container', {
-        direction: 'horizontal', // 切换选项
-        autoplay: true,
-        loop: true, // 循环模式选项
-        delay: 5000,//5秒切换一次
-
-        // 如果需要分页器
-        pagination: {
-            el: '.swiper-pagination',
-            clickable :true,
-        },
-        // 如果需要前进后退按钮
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
-        // 如果需要滚动条
-        scrollbar: {
-            el: '.swiper-scrollbar',
-        },
-    })
 
     /**
      * 登陆页面
