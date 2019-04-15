7a8bb3c324aa41facce60c23d0e3b7c1cf739fb2
duguangyan <dis2010@126.com>
2019-04-15 18:39:46 +0800

    x

--- routes/index.js
+++ routes/index.js
@@ -18,7 +18,10 @@ router.get('/', function(req, res, next) {
 
 
 router.get('/wx', function(req, res, next) {
-
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Headers', 'Content-type');
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS,PATCH");
+    res.header('Access-Control-Max-Age',1728000);//预请求缓存20天
 
     let nowUrl = req.url;
 
